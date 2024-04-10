7f8c47a97408024016150438c66a52a0506320f3
AbhiramAv <abhiramav04@gmail.com>
2024-04-10 13:58:59 -0400

    initial-commit

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/AbhiramAv" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Masters in Computer Science degree. I hung it on the wall because I'm proud of it.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I've like to play games. Here are some of my gaming profiles: 
+   <a href="https://psnprofiles.com/JOk3rrr_19" target="_blank">PSProfile</a>`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="/abhiram_av.pdf" target="_blank">Check it out?</a>
+  Contact me at abhiramav04@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. `,
+  exit: `If you want to exit Abhiram's portfolio, just close the tab.`,
+};
